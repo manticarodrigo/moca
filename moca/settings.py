1c5352888515e48e5fa5ffb2fa26f6bdbf72da23
ElHassan Wanas <ewanas@tutanota.com>
2019-06-22 02:01:00 +0300

    Adding more stuff based on the tutorial

--- moca/settings.py
+++ moca/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'moca_app.apps.MocaAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ moca_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MocaAppConfig(AppConfig):
+    name = 'moca_app'
