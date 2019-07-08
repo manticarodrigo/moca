bee12ccc8cddff12bc3992fe794a5539627a391f
Tarek Abdelhamid <tee@MacBook-Pro.fios-router.home>
2019-07-08 13:38:04 -0400

    Support enviroment specific settings

---
+++ apps/config/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True
+
+# Secret key for the dev environment
+SECRET_KEY = 'fi_nlfw&)!t_ep1c$q435a+!)q*8a4kr$b_#w#j5gj!2#0q^5n'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moca_db',
+        'USER': 'moca_user',
+        'PASSWORD': 'moca_password',
+        'HOST': 'db', # set in docker-compose.yml
+        'PORT': 5432
+    }
+}
