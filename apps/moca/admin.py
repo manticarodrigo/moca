90a231370966585eb6917ba8f217d9823711dc73
ElHassan Wanas <ewanas@approdite.com>
2019-08-21 20:22:34 +0300

    Adding own user class instead of the default

--- apps/moca/admin.py
+++ apps/moca/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Conversation, Message, Participant
+from .models import Conversation, Message, Participant, User
 
+admin.site.register(User)
 admin.site.register(Conversation)
 admin.site.register(Message)
 admin.site.register(Participant)
