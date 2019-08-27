ed0e3505711efe5a479f55d4ced03a6ea7ee18cf
El-Hassan Wanas <ewanas@approdite.com>
2019-08-27 14:36:47 +0300

    Correct serialization behaviour, still need to figure out if address is required or not, but the previous code was definitely off :)

--- apps/moca/api/user/serializers.py
+++ apps/moca/api/user/serializers.py
@@ -8,16 +8,35 @@ User = get_user_model()
 class AddressSerializer(serializers.ModelSerializer):
   class Meta:
     model = Address
-    fields = ('name', 'text', 'location', 'primary', 'apartment')
+    exclude = ['user']
+
+  def create(self, validated):
+    return Address.objects.create(**validated)
 
 
 class UserSerializer(serializers.ModelSerializer):
-  addresses = AddressSerializer(many=True)
-  id = serializers.IntegerField(read_only=True)
-  type = serializers.ChoiceField(choices=User.USER_TYPES, required=True)
-  gender = serializers.ChoiceField(choices=User.GENDERS, required=True)
+  addresses = AddressSerializer(many=True, required=True)
 
   class Meta:
     model = User
-    fields = ('id', 'email', 'first_name', 'last_name', 'gender', 'date_of_birth', 'created_at',
-              'type', 'addresses')
+    fields = '__all__'
+    depth = 1
+    extra_kwargs = {'password': {'write_only': True}}
+
+  def create(self, validated):
+    addresses = validated.pop('addresses')
+    user = User.objects.create_user(**validated)
+
+    for address in addresses:
+      address = Address.objects.create(user=user, **address)
+      user.addresses.add(address)
+
+    return user
+
+
+class UserSerializerForRead(serializers.ModelSerializer):
+  addresses = AddressSerializer(many=True)
+
+  class Meta:
+    model = User
+    exclude = ['password']
--- apps/moca/api/user/views.py
+++ apps/moca/api/user/views.py
@@ -9,35 +9,14 @@ class UserAPIView(generics.RetrieveAPIView):
   permission_classes = [
     permissions.IsAuthenticated,
   ]
-  serializer_class = UserSerializer
 
-  def get(self, request):
-    return Response({})
+  serializer_class = UserSerializer
 
   @transaction.atomic
   def post(self, request):
     serializer = self.get_serializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    validated = serializer.validated_data
 
-    user = User.objects.create_user(first_name=validated['first_name'],
-                                    last_name=validated['last_name'],
-                                    gender=validated['gender'],
-                                    date_of_birth=validated['date_of_birth'],
-                                    type=validated['type'],
-                                    email=validated['email'])
-
-    user.save()
-
-    addresses = [
-      Address.objects.create(name=address['name'],
-                             text=address['text'],
-                             location=address['location'],
-                             primary=address['primary'],
-                             user=user) for address in validated['addresses']
-    ]
-
-    for address in addresses:
-      address.save()
+    serializer.save()
 
     return Response(serializer.data)
