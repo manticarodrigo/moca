42ac4d63ec1f1696bc0eb00ccf8866288e311ea1
El-Hassan Wanas <ewanas@approdite.com>
2019-09-27 01:20:03 +0300

    Truncating all database tables before running all tests

--- integration/run_tests.sh
+++ integration/run_tests.sh
@@ -2,6 +2,11 @@
 
 if [ -z ${service+x} ]; then
   export service="http://localhost:8000"
+  runCleanup="1"
 fi
 
-(cd integration/tests; PYTHONPATH=$(pwd) py.test)
+if [ -n runCleanup ]; then
+  echo "truncate moca_user cascade" | docker exec -i moca_service python apps/manage.py dbshell
+fi
+
+(cd integration/tests; PYTHONPATH=$(pwd) py.test $@)
