3a229cbedd5e797003fd559b2c9beb6ebea53b9a
Ugur <ugurkaya7@gmail.com>
2019-09-28 18:19:59 +0300

    Appointment integration test added. /nIntegration tests util.py data storing is changed.

--- integration/run_tests.sh
+++ integration/run_tests.sh
@@ -4,6 +4,4 @@ if [ -z ${service+x} ]; then
   export service="http://localhost:8000"
 fi
 
-cd integration/tests;
-PYTHONPATH=$PYTHONPATH:$(pwd) 
-py.test ./test_*.yaml
+(cd integration/tests; PYTHONPATH=$(pwd) py.test)
