3e2bf4a708d9c4f8c10753abfab36d579b92e6dc
El-Hassan Wanas <ewanas@approdite.com>
2019-10-22 23:38:05 +0300

    Fix for multiple prices for the same session type for the same therapist

--- apps/moca/models/prices.py
+++ apps/moca/models/prices.py
@@ -7,3 +7,6 @@ class Price(models.Model):
   therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE, related_name="tariffs")
   session_type = models.CharField(max_length=20)
   price = models.PositiveIntegerField()
+
+  class Meta:
+    unique_together = ['therapist', 'session_type']
