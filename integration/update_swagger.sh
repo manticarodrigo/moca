e85ba6d02bbdf0c9800608ff80713a799739fe0e
El-Hassan Wanas <ewanas@approdite.com>
2019-10-29 19:41:57 +0300

    fixup! More debugging

--- integration/update_swagger.sh
+++ integration/update_swagger.sh
@@ -4,4 +4,4 @@ if [ -z ${service+x} ]; then
   export service="http://localhost:8000"
 fi
 
-http ${service}/api/docs/swagger.yaml > swagger.yaml && git diff swagger.yaml
+http "${service}/api/docs/swagger.yaml" > swagger.yaml && git diff swagger.yaml
