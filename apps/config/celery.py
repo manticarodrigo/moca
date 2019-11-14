2a85d8e044479bdb41893b2d492d7ab8bc9f6310
Tarek Abdelhamid <tee@Tarek-Mac-Mini.local>
2019-11-14 17:33:09 -0500

    Remove unused import

--- apps/config/celery.py
+++ apps/config/celery.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import, unicode_literals
-import os, sys
+import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
-sys.path.append(os.path.abspath('config'))
 app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
